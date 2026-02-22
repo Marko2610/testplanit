5d28e82573f28ca35212d3fdcc445276fb711edc
Brad DerManouelian <bderman@gmail.com>
2026-02-22 12:57:00 -0600

    chore: update dependencies in package.json and pnpm-lock.yaml
    - Bump systeminformation to version 5.31.1
    - Update hono to version 4.11.10
    - Upgrade jspdf to version 4.2.0
    - Adjusted regex in tiptapConversion.ts for improved HTML parsing

--- testplanit/utils/tiptapConversion.ts
+++ testplanit/utils/tiptapConversion.ts
@@ -237,7 +237,7 @@ export const ensureTipTapJSON = (value: any): JSONContent => {
       }
     }
 
-    if (/<\/?[a-z][\s\S]*>/i.test(trimmed)) {
+    if (/<\/?[a-z][^>]*>/i.test(trimmed)) {
       return convertHtmlToTipTapJSON(trimmed);
     }
 
